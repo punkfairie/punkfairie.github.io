f83e11bfb7d49ace2fec7dcc46462f2ebc760ab5
marleyrae <marleyrae@protonmail.com>
2023-06-29 09:30:29 -0700

    Add Sass processor

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,10 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(require('eleventy-sass'), {
+    postcss: require('postcss')([
+      require('postcss-preset-env'),
+      require('cssnano'),
+    ]),
+  })
+
   return {dir: {input: 'src', output: 'dist'}}
 }
