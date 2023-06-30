1abd43a2aca6b693ff895f138273c7394ebbeb10
marleyrae <marleyrae@protonmail.com>
2023-06-29 21:05:45 -0700

    SCSS -> PostCSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,25 @@
+const fs = require('fs')
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(require('eleventy-sass'), {
-    postcss: require('postcss')([
-      require('postcss-preset-env'),
-      require('cssnano'),
-    ]),
+  eleventyConfig.on('eleventy.before', async () => {
+    const cssSrc = './src/style/style.css'
+    const cssDest = './dist/style.css'
+
+    fs.readFile(cssSrc, (err, css) => {
+      require('postcss')([
+        require('postcss-import-ext-glob'),
+        require('postcss-import'),
+        require('postcss-preset-env'),
+        require('cssnano'),
+      ])
+        .process(css, {from: cssSrc, to: cssDest})
+        .then(res => {
+          fs.writeFile(cssDest, res.css, () => true)
+        })
+    })
   })
+
+  eleventyConfig.addWatchTarget('./src/style/**/*.css')
 
   eleventyConfig.addPlugin(require('eleventy-plugin-automatic-noopener'), {
     noreferrer: true,
